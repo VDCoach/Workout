42cabae1d35607922e1723a3ce6cdbb0fcf60402
David <david.voisard2@gmail.com>
2026-05-05 15:38:56 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,19 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    // Indispensable pour que les ressources (JS/CSS) soient trouvées sur https://vdcoach.github.io/Workout/
-    base: '/Workout/',[cite: 3]
+    // Indispensable pour GitHub Pages
+    base: '/Workout/',
     
-    plugins: [react(), tailwindcss()],[cite: 3]
+    plugins: [react(), tailwindcss()],
     
     resolve: {
       alias: {
-        // Définit '@' comme alias pointant vers la racine du projet
-        '@': path.resolve(__dirname, '.'),[cite: 3]
+        '@': path.resolve(__dirname, '.'),
       },
     },
     
     server: {
-      // Désactive le HMR (Hot Module Replacement) si la variable d'environnement DISABLE_HMR est à 'true'
-      hmr: process.env.DISABLE_HMR !== 'true',[cite: 3]
+      hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
 });
